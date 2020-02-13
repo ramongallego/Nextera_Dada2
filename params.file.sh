a470e7bf5e8f5080347a3449605a094253f5b1bb
ramongallego <ramongallego@gmail.com>
2020-02-13 13:36:08 -0800

    Improved dialogues

--- params.file.sh
+++ params.file.sh
@@ -5,7 +5,7 @@
 # INPUT
 ################################################################################
 # What is the file path to the directory containing all of the libraries/reads?
-PARENT_DIR="${MAIN_DIR}/data"
+PARENT_DIR="${MAIN_DIR}/data_sub"
 
 # Where is the sequencing metadata file? (SEE FORMATTING GUIDELINES IN README!)
 SEQUENCING_METADATA="${PARENT_DIR}"/metadata.csv
