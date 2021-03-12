be47612885c0bdb020a85cb73f5d91d4fe67e5a4
ramongallego <ramongallego@gmail.com>
2021-03-12 13:18:42 -0800

    work params with relative paths

--- scripts/submit.slurm.dada2.sh
+++ scripts/submit.slurm.dada2.sh
@@ -17,9 +17,9 @@ echo this works
 module load R
 
 # Move to the correct folders
-cd /home/rgallegosimon/
+#cd /home/rgallegosimon/
 
 # Run R Script
 # R CMD BATCH pipeline/Nextera_Dada2/scripts/skeleton_dada2.r "${1}"
-Rscript --vanilla pipeline/Nextera_Dada2/scripts/skeleton_dada2.r  "${1}"
+Rscript --vanilla /home/rgallegosimon/pipeline/Nextera_Dada2/scripts/skeleton_dada2.r  "${1}"
  
